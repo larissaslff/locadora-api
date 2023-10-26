af8bf7eac4aa238a6ab0ee83e5b1bcc2a2aaa974
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-10-25 21:02:33 -0300

    add test in clientecontroller to the buscarTodos method

--- cine/src/test/java/com/locadora/cine/controllers/ClienteControllerTest.java
+++ cine/src/test/java/com/locadora/cine/controllers/ClienteControllerTest.java
@@ -1,9 +1,14 @@
 package com.locadora.cine.controllers;
 
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,17 +18,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.locadora.cine.models.Cliente;
 import com.locadora.cine.services.ClienteServiceImpl;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
+import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ClienteControllerTest {
+
+    /**
+     *
+     */
+    private static final String V1_CLIENTES = "/v1/clientes";
 
     @MockBean
     private ClienteServiceImpl clienteService;
@@ -34,18 +46,23 @@ public class ClienteControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private static Cliente cliente;
+
+    @BeforeAll
+    static void setUp() {
+        cliente = Cliente.builder()
+                .nome("Nome")
+                .sobrenome("Sobrenome")
+                .endereco("Endereço")
+                .build();
+    }
+
     @Test
     void deveSalvarCliente() throws JsonProcessingException, Exception {
 
-        Cliente cliente = Cliente.builder()
-        .nome("Nome")
-        .sobrenome("Sobrenome")
-        .endereco("Endereço")
-        .build();
-
         when(clienteService.save(cliente)).thenReturn(new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED));
 
-        mockMvc.perform(post("/v1/clientes")
+        mockMvc.perform(post(V1_CLIENTES)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(cliente)))
                 .andExpect(status().isCreated())
@@ -55,5 +72,30 @@ public class ClienteControllerTest {
                 .andExpect(jsonPath("$.telefone").value(cliente.getTelefone()))
                 .andExpect(jsonPath("$.alugueis").value(cliente.getAlugueis()))
                 .andReturn();
+    }
+
+    @Test
+    void deveBuscarTodosOsClientes() throws Exception {
+
+        Cliente cliente02 = cliente = Cliente.builder()
+                .nome("Nome2")
+                .sobrenome("Sobrenome2")
+                .endereco("Endereço2")
+                .telefone("telefone")
+                .build();
+
+        ResponseEntity<List<Cliente>> serviceResponse = ResponseEntity.ok(List.of(cliente, cliente02));
+
+        when(clienteService.buscarClientes()).thenReturn(serviceResponse);
+
+        mockMvc.perform(get(V1_CLIENTES)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].nome").value(cliente.getNome()))
+                .andExpect(jsonPath("$[0].telefone").value(cliente.getTelefone()))
+                .andExpect(jsonPath("$[1].nome").value(cliente02.getNome()))
+                .andExpect(jsonPath("$[1].telefone").value(cliente02.getTelefone()));
     }
 }
