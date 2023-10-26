dd284c07bb53a840450b1df932f678ee2ac9280d
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-10-25 21:41:22 -0300

    using tdd to create buscarPorId method in service

--- cine/src/test/java/com/locadora/cine/services/ClienteServiceImplTest.java
+++ cine/src/test/java/com/locadora/cine/services/ClienteServiceImplTest.java
@@ -1,8 +1,10 @@
 package com.locadora.cine.services;
 
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
@@ -19,66 +21,80 @@ import com.locadora.cine.repositories.ClienteRepository;
 @ExtendWith(MockitoExtension.class)
 public class ClienteServiceImplTest {
 
-    @Mock
-    private ClienteRepository clienteRepository;
+        @Mock
+        private ClienteRepository clienteRepository;
 
-    @InjectMocks
-    private ClienteServiceImpl clienteService;
+        @InjectMocks
+        private ClienteServiceImpl clienteService;
+
+        private static Cliente cliente;
+
+        static void setUp() {
+                cliente = Cliente.builder()
+                                .nome("Cliente 01")
+                                .sobrenome("Cliente 01")
+                                .telefone("telefone")
+                                .endereco("endereco")
+                                .build();
+        }
+
+        @Test
+        void deveSalvarCliente() {
+
+                Cliente clienteASalvar = Cliente.builder()
+                                .nome("Cliente nome")
+                                .sobrenome("Cliente sobrenome")
+                                .telefone("telefone")
+                                .endereco("endereco")
+                                .build();
+
+                Cliente clienteSalvo = Cliente.builder()
+                                .id(1L)
+                                .nome("Cliente nome")
+                                .sobrenome("Cliente sobrenome")
+                                .telefone("telefone")
+                                .endereco("endereco")
+                                .build();
+
+                when(clienteRepository.save(clienteASalvar)).thenReturn(clienteSalvo);
+
+                ResponseEntity<Cliente> actual = clienteService.save(clienteASalvar);
+
+                assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+                assertThat(actual.getBody().getId()).isNotNull();
+                assertThat(actual.getBody().getNome()).isEqualTo(clienteASalvar.getNome());
+
+        }
+
+        @Test
+        void deveRetornarTodosClientes() {
+
+                Cliente cliente02 = Cliente.builder()
+                                .nome("Cliente 02")
+                                .sobrenome("Cliente 02")
+                                .telefone("telefone")
+                                .endereco("endereco")
+                                .build();
+
+                List<Cliente> clientes = List.of(cliente, cliente02);
+
+                when(clienteRepository.findAll()).thenReturn(clientes);
+
+                ResponseEntity<List<Cliente>> actual = clienteService.buscarClientes();
+
+                assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.OK);
+                assertThat(actual.getBody().size()).isEqualTo(clientes.size());
+                assertThat(actual.getBody().get(0).getNome()).isEqualTo(cliente.getNome());
+                assertThat(actual.getBody().get(1).getSobrenome()).isEqualTo(cliente02.getSobrenome());
+
+        }
 
     @Test
-    void deveSalvarCliente() {
+    void deveRetornarClienteQuandoExistentePorId(){
+        when(clienteRepository.findById(anyLong())).thenReturn(Optional.of(cliente));
 
-        Cliente clienteASalvar = Cliente.builder()
-                .nome("Cliente nome")
-                .sobrenome("Cliente sobrenome")
-                .telefone("telefone")
-                .endereco("endereco")
-                .build();
+        ResponseEntity<Cliente> clienteEncontrado = clienteService.buscarClientePorId(1L);
 
-        Cliente clienteSalvo = Cliente.builder()
-                .id(1L)
-                .nome("Cliente nome")
-                .sobrenome("Cliente sobrenome")
-                .telefone("telefone")
-                .endereco("endereco")
-                .build();
-
-        when(clienteRepository.save(clienteASalvar)).thenReturn(clienteSalvo);
-
-        ResponseEntity<Cliente> actual = clienteService.save(clienteASalvar);
-
-        assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(actual.getBody().getId()).isNotNull();
-        assertThat(actual.getBody().getNome()).isEqualTo(clienteASalvar.getNome());
-        
-    }
-
-    @Test
-    void deveRetornarTodosClientes() {
-        Cliente cliente01 = Cliente.builder()
-                .nome("Cliente 01")
-                .sobrenome("Cliente 01")
-                .telefone("telefone")
-                .endereco("endereco")
-                .build();
-
-        Cliente cliente02 = Cliente.builder()
-                .nome("Cliente 02")
-                .sobrenome("Cliente 02")
-                .telefone("telefone")
-                .endereco("endereco")
-                .build();
-
-        List<Cliente> clientes = List.of(cliente01, cliente02);
-
-        when(clienteRepository.findAll()).thenReturn(clientes);
-
-        ResponseEntity<List<Cliente>> actual = clienteService.buscarClientes();
-
-        assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(actual.getBody().size()).isEqualTo(clientes.size());
-        assertThat(actual.getBody().get(0).getNome()).isEqualTo(cliente01.getNome());
-        assertThat(actual.getBody().get(1).getSobrenome()).isEqualTo(cliente02.getSobrenome());
-
+        assertThat(clienteEncontrado.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
