848cb9af52723004967f6dece6235f4702960d23
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-10-26 21:31:41 -0300

    using tdd to implement deletarCliente in controller

--- cine/src/test/java/com/locadora/cine/controllers/ClienteControllerTest.java
+++ cine/src/test/java/com/locadora/cine/controllers/ClienteControllerTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
@@ -17,6 +18,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
@@ -153,6 +155,16 @@ public class ClienteControllerTest {
         mockMvc.perform(put(V1_CLIENTES + "/100")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(clienteAtualizado)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.nome").doesNotExist());
+    }
+
+    @Test
+    void deveDeletarCliente() throws Exception {
+        when(clienteService.deletarCliente(100L)).thenReturn(new ResponseEntity<Void>(HttpStatus.NO_CONTENT));
+        
+        mockMvc.perform(delete(V1_CLIENTES + "/100")
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.nome").doesNotExist());
     }
